9bf2525f85cd1abf9005385ee3dc4740a00e974b
Anthony Romrell <anthony@artisandream.com>
2017-11-04 16:13:49 -0600

    WeaponListRedo

--- Unity/Shrimp2017/Assets/Scripts/Weapons/WeaponList.cs
+++ Unity/Shrimp2017/Assets/Scripts/Weapons/WeaponList.cs
@@ -6,145 +6,142 @@ using UnityEngine.UI;
 
 public class WeaponList : MonoBehaviour
 {
-    public Color[] WeaponColor;
-    public static Action<int> ActivateWeaponSwitch;
-    public static Action WeaponType;
-    public Vector3 resetWeaponRotation;
-    public Transform meleeAttachPoint;
-    public Transform RangeAttachPoint;
-    private int adderNum;
-    public int currentWeaponNum = 0;
-    public int currentlyPickedWeaponNum = 0;
-    public Vector3 weaponLocation;
 
-    void CurrentWeaponNum(int _i)
-    {
-        currentWeaponNum = _i;
-    }
+	public List<WeaponClass> avaliableWeapons;
+	public List<WeaponBar> WeaponBars;
+	public List<FireWeaponChoice> FireButtons;
+	public Color[] WeaponColor;
+	public static Action<int> ActivateWeaponSwitch;
+	public static Action WeaponType;
+	public Vector3 resetWeaponRotation;
+	public Transform meleeAttachPoint;
+	public Transform RangeAttachPoint;
+	private int adderNum;
+	public int currentWeaponNum = 0;
+	public int currentlyPickedWeaponNum = 0;
+	public Vector3 weaponLocation;
 
-    public void Start()
-    {
-        GameData.Instance.WeaponBars = new List<WeaponBar>();
-        GameData.Instance.WeaponsList = new List<WeaponClass>();
-        GameData.Instance.currentWeapon = null;
-        GameData.Instance.FireButtons = new List<FireWeaponChoice>();
+	void CurrentWeaponNum(int _i)
+	{
+		currentWeaponNum = _i;
+	}
 
-    
-        WeaponBar.AddWeaponBar += AddWeaponBars;
-        WeaponAttachPoint.AttachAction += addAttachPoints;
-        resetWeaponRotation.x = 270;
-        FireWeaponChoice.AddFire += AddWeaponButtons;
-        WeaponClass.AddWeaponToList += AddWeapons;
-        SwitchCurrentWeapon.SwitchWeapon += SwitchThisWeapon;
-        FireWeaponChoice.FireCurrentWeapon += CurrentWeaponNum;
-        FireWeaponChoice.FireCurrentWeapon += FireWeapon;
-    }
+	public void Start()
+	{
+		WeaponAttachPoint.AttachAction += addAttachPoints;
+		resetWeaponRotation.x = 270;
+		WeaponBars = new List<WeaponBar>();
+		WeaponBar.AddWeaponBar += AddWeaponBars;
+		FireWeaponChoice.AddFire += AddWeaponButtons;
+		avaliableWeapons = new List<WeaponClass>();
+		WeaponClass.AddWeaponToList += AddWeapons;
+		SwitchCurrentWeapon.SwitchWeapon += SwitchThisWeapon;
+		FireWeaponChoice.FireCurrentWeapon += CurrentWeaponNum;
+		FireWeaponChoice.FireCurrentWeapon += FireWeapon;
+		//FireThisWeapon.FireCurrentWeapon += FireWeapon;
+	}
 
-    void AddWeaponBars(WeaponBar _weaponBar)
-    {
-        GameData.Instance.WeaponBars.Add(_weaponBar);
-    }
+	void AddWeaponBars(WeaponBar _b)
+	{
+		WeaponBars.Add(_b);
+	}
 
-    void addAttachPoints(WeaponAttachPoint obj)
-    {
-        switch (obj.thisAttachType)
-        {
-            case WeaponAttachPoint.AttachType.Melee:
-                meleeAttachPoint = obj.transform;
-                break;
+	void addAttachPoints(WeaponAttachPoint obj)
+	{
+		switch (obj.thisAttachType) {
+			case WeaponAttachPoint.AttachType.Melee:
+				meleeAttachPoint = obj.transform;
+				break;
 
-            case WeaponAttachPoint.AttachType.Range:
-                RangeAttachPoint = obj.transform;
-                break;
-        }
-    }
+			case WeaponAttachPoint.AttachType.Range:
+				RangeAttachPoint = obj.transform;
+				break;
+		}
+	}
 
-    void AddWeaponButtons(FireWeaponChoice _f)
-    {
-        GameData.Instance.FireButtons.Add(_f);
-    }
+	void AddWeaponButtons(FireWeaponChoice _f)
+	{
+		FireButtons.Add(_f);
+	}
 
-    void AddBarToWeaponClass(WeaponClass _w)
-    {
-        adderNum = GameData.Instance.WeaponsList.Count - 1;
-        GameData.Instance.WeaponsList[adderNum].ChangeAmmoBar += GameData.Instance.WeaponBars[adderNum].UpdateAmmoBar;
-        GameData.Instance.WeaponBars[adderNum].GetComponent<Image>().color = WeaponColor[adderNum];
+	void AddBarToWeaponClass(WeaponClass _w)
+	{
+		adderNum = avaliableWeapons.Count - 1;
+		avaliableWeapons [adderNum].ChangeAmmoBar += WeaponBars [adderNum].UpdateAmmoBar;
+		WeaponBars [adderNum].GetComponent<Image>().color = WeaponColor [adderNum];
 
-        GameData.Instance.FireButtons[adderNum].choiceNum = adderNum;
-        GameData.Instance.FireButtons[adderNum].GetComponent<Image>().color = WeaponColor[adderNum];
-        GameData.Instance.FireButtons[adderNum].GetComponent<Image>().enabled = true;
-    }
+		FireButtons [adderNum].choiceNum = adderNum;
+		FireButtons [adderNum].GetComponent<Image>().color = WeaponColor [adderNum];
+		FireButtons [adderNum].GetComponent<Image>().enabled = true;
+	}
 
-    void AddWeapons(WeaponClass _w)
-    {
-        resetWeaponRotation.y = _w.setRotation;
-        GameData.Instance.WeaponsList.Add(_w);
-        if (ActivateWeaponSwitch != null)
-            ActivateWeaponSwitch(GameData.Instance.WeaponsList.Count);
+	void AddWeapons(WeaponClass _w)
+	{
+		resetWeaponRotation.y = _w.setRotation;
+		avaliableWeapons.Add(_w);
+		if (ActivateWeaponSwitch != null)
+			ActivateWeaponSwitch(avaliableWeapons.Count);
 
-        if (WeaponType != null)
-            WeaponType();
+		if (WeaponType != null)
+			WeaponType();
 
-        AddBarToWeaponClass(_w);
+		AddBarToWeaponClass(_w);
 
-        switch (_w.KindOfWeapon)
-        {
-            case WeaponAttachPoint.AttachType.Melee:
-                _w.gameObject.transform.parent = meleeAttachPoint;
-                break;
+		switch (_w.KindOfWeapon) {
+			case WeaponAttachPoint.AttachType.Melee:
+				_w.gameObject.transform.parent = meleeAttachPoint;
+				break;
+			
+			case WeaponAttachPoint.AttachType.Range:
+				_w.gameObject.transform.parent = RangeAttachPoint;
+				break;
+		}
 
-            case WeaponAttachPoint.AttachType.Range:
-                _w.gameObject.transform.parent = RangeAttachPoint;
-                break;
-        }
+		_w.gameObject.transform.localPosition = weaponLocation;
+		_w.gameObject.transform.localRotation = Quaternion.Euler(resetWeaponRotation);
+		DeactivateAllWeapons();
+	}
 
-        _w.gameObject.transform.localPosition = weaponLocation;
-        _w.gameObject.transform.localRotation = Quaternion.Euler(resetWeaponRotation);
-        DeactivateAllWeapons();
-    }
+	void DeactivateAllWeapons()
+	{
+		foreach (WeaponClass _w in avaliableWeapons) {
+			_w.gameObject.SetActive(false);
+		}
+		ActivateCurrentWeapon();
+	}
 
-    void DeactivateAllWeapons()
-    {
-        foreach (WeaponClass _weaponClass in GameData.Instance.WeaponsList)
-        {
-            _weaponClass.gameObject.SetActive(false);
-        }
-        ActivateCurrentWeapon();
-    }
+	void ActivateCurrentWeapon()
+	{
+		avaliableWeapons [currentWeaponNum].gameObject.SetActive(true);
+	}
 
-    void ActivateCurrentWeapon()
-    {
-        GameData.Instance.currentWeapon = GameData.Instance.WeaponsList[currentWeaponNum];
-        GameData.Instance.currentWeapon.gameObject.SetActive(true);
-    }
+	void SwitchThisWeapon()
+	{
+		if (avaliableWeapons.Count - 1 > currentWeaponNum) {
+			currentWeaponNum++;
+		} else {
+			currentWeaponNum = 0;
+		}
+	}
 
-    void SwitchThisWeapon()
-    {
-        if (GameData.Instance.WeaponsList.Count - 1 > currentWeaponNum)
-        {
-            currentWeaponNum++;
-        }
-        else
-        {
-            currentWeaponNum = 0;
-        }
-    }
+	void Fire()
+	{
+		if (avaliableWeapons.Count > 0) {
+			//if (currentlyPickedWeaponNum == currentWeaponNum) {
+				DeactivateAllWeapons();
+			//}
+			avaliableWeapons [currentWeaponNum].FireAmmo();
+		}
 
-    void Fire()
-    {
-        if (GameData.Instance.currentWeapon != null)
-        {
-            GameData.Instance.currentWeapon.FireAmmo();
-        }
+	}
+	void FireWeapon(int _i)
+	{
+		currentlyPickedWeaponNum = _i;
+		Fire();
+	}
 
-    }
-    void FireWeapon(int _i)
-    {
-        currentlyPickedWeaponNum = _i;
-        Fire();
-    }
-    void FireWeapon()
-    {
-        Fire();
-    }
+	void FireWeapon()
+	{
+		Fire();
+	} 
 }
