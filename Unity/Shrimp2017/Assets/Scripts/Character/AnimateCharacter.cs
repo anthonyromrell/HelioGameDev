fae8400c1145a7e10a1872c09ce1af13e6c06465
Anthony Romrell <anthonyromrell@mac.com>
2017-11-13 13:54:09 -0700

    Updating more sounds

--- Unity/Shrimp2017/Assets/Scripts/Character/AnimateCharacter.cs
+++ Unity/Shrimp2017/Assets/Scripts/Character/AnimateCharacter.cs
@@ -14,6 +14,11 @@ public class AnimateCharacter : MonoBehaviour
     public Animator characterAnim;
     public Weapon weapon;
     public GameObject onFX;
+    public AudioSource soundFX;
+
+    public void Step () {
+        soundFX.Play();
+    }
    
     void Start()
     {
