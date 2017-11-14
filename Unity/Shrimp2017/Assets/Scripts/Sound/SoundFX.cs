683ae507364fbc02630a978e46f137a641f8fb6d
Anthony Romrell <anthony@artisandream.com>
2017-11-13 19:04:08 -0700

    Adding Audio Assets

---
+++ Unity/Shrimp2017/Assets/Scripts/Sound/SoundFX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundFX : MonoBehaviour {
+
+	public AudioSource enemyMusic;
+	public AudioMixer musicMixer;
+	void Start () {
+		
+	}
+	
+	public void ChangeSound () {
+		
+	}
+
+}
